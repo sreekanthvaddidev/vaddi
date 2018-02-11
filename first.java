7633d30184ecb1a99fd32582752fa76522b1702d
sreekanthvaddidev <sreekanth.yks@gmail.com>
2018-02-11 03:32:00 -0800

    Teat java programe

---
+++ first.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+public class first {
+   public static void main(String[] args) {
+//      Prints "Hello, World" in the terminal window.
+             System.out.println("Hello, World");
+                }
+                }
